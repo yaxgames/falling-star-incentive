69478cfbeca786c92b1edc61eaeda5a14a703880
Anthony <yaxgames@gmail.com>
2016-05-26 20:03:35 -0400

    Added blog creator
    Added a form to add a new blog. tweaked function to add to collection.

--- falling-star/client/main.js
+++ falling-star/client/main.js
@@ -6,7 +6,7 @@ import './main.html';
 //Collections shared in the client 
 Blogs = new Mongo.Collection("blogs");
 Users = new Mongo.Collection("users");
-CurrentUser = "";
+CurrentUser = 0;
 Template.blogdisplay.helpers({
 	Blogs: function(){
 		return Blogs.find({}, {sort: {datetime:-1}});
@@ -15,17 +15,19 @@ Template.blogdisplay.helpers({
 
 Template.login.events({
 	"submit .login": function(e){
+	console.log("test login");
 		var loginattempt = [e.target.user.value,e.target.pass.value];
 		//to proceed with the login check
 		return false;
 	}
 });
 
-Template.login.events({
+Template.blogcreate.events({
 	//Creating new blogs via the create form
 	"submit .blogcreate": function(e){
+	console.log("new blog: " + e.target.title.value);
 		var blogentry = [e.target.title.value,e.target.desc.value];
-		var cUserID = Users.find({username: CurrentUser}).fetch()[0].userid;
+		var cUserID = 0//Users.find({username: CurrentUser}).fetch()[0].userid;
 		Blogs.insert({title: blogentry[0], description: blogentry[1],datetime: Date(), userid: cUserID});
 		
 		return false;
