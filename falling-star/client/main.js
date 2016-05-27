baf659b76211bbfdf73b80cc41db19d94d662217
Anthony <yaxgames@gmail.com>
2016-05-26 20:47:18 -0400

    added filtering of blog entry
    Added disabling blog entry if no user logged in.
    Added disabling log in if user is logged in.

--- falling-star/client/main.js
+++ falling-star/client/main.js
@@ -7,12 +7,36 @@ import './main.html';
 Blogs = new Mongo.Collection("blogs");
 Users = new Mongo.Collection("users");
 CurrentUser = 0;
+userloggedin = false;
+//helpers
 Template.blogdisplay.helpers({
 	Blogs: function(){
 		return Blogs.find({}, {sort: {datetime:-1}});
 	}
 });
+Template.login.helpers({
+	'nouser':function(){	
+		if (CurrentUser == 0 || CurrentUser == undefined){
+			console.log("no user logged in");
+			return true;
+		}
+		console.log("user logged in " + CurrentUser);
+		return false;
+	}
+	
+});
+Template.blogcreate.helpers({
+	'userlogged': function(){
+		if (CurrentUser != 0 && CurrentUser != undefined){
+		console.log("user logged in " + CurrentUser);
+			return true;
+		}
+		console.log("No user logged in ");
+		return false;
+	}
+});
 
+//events
 Template.login.events({
 	"submit .login": function(e){
 	console.log("test login");
@@ -27,7 +51,7 @@ Template.blogcreate.events({
 	"submit .blogcreate": function(e){
 	console.log("new blog: " + e.target.title.value);
 		var blogentry = [e.target.title.value,e.target.desc.value];
-		var cUserID = 0//Users.find({username: CurrentUser}).fetch()[0].userid;
+		var cUserID = 0;//Users.find({username: CurrentUser}).fetch()[0].userid;
 		Blogs.insert({title: blogentry[0], description: blogentry[1],datetime: Date(), userid: cUserID});
 		
 		return false;
