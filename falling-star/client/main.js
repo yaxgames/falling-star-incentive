83ca239346a9dd88c7f07768fd21cca02fd764f2
Anthony <yaxgames@gmail.com>
2016-05-30 21:59:13 -0400

    Cleaned up a bit in the code
    Cleaned up the code a bit, added further detailed comments.
    added the hide edit form functionally
    centered blog to make it look readable.

--- falling-star/client/main.js
+++ falling-star/client/main.js
@@ -9,13 +9,16 @@ Users = new Mongo.Collection("users");
 //Creating session to hold temporary variables such as current user logged in.
 Session.set("currentuser","");
 Session.set('registerdisplay',false);
-userloggedin = false;
-//helpers
+Session.set('editdisplay',false);
+
+//------------------helpers-----------------
+//----Blog Display Helper, to display the blog in order of date---
 Template.blogdisplay.helpers({
 	Blogs: function(){
 		return Blogs.find({}, {sort: {datetime:-1}});
 	}
 });
+//------------the blog display helpers for form variables
 Template.blogdisplay.helpers({
 	'sameuser':function(bloguser){
 		if(!Session.equals("currentuser","")){
@@ -24,15 +27,15 @@ Template.blogdisplay.helpers({
 			}
 			return false;
 		}
-	}
-});
-
-Template.blogdisplay.helpers({
+	},
 	'dateformat':function(date){
 		return moment(date).format('YYYY-MMM-DD h:mm:ss a');
+	},
+	'editdisplay':function(){
+		return Session.get("editdisplay");
 	}
 });
-
+//----Helpers to determine if users are logged in---
 Template.login.helpers({
 	'nouser':function(){	
 		if (Session.equals("currentuser","")){
@@ -66,39 +69,48 @@ Template.blogcreate.helpers({
 	}
 });
 
+//----helper to determine if the registration should display
 Template.userregister.helpers({
 	'registerdisplay':function(){
 		return Session.get('registerdisplay');
 	}
 });
-//events
+
+
+//----------------------------events----------------------------
+//---Log in event----
 Template.login.events({
+//on login button
 	"submit .login": function(e){
-	
+	//collect the form values into array
 		var loginattempt = [e.target.user.value,e.target.pass.value];
+		//if user exists check password
 		if(Users.find({username: loginattempt[0]}).fetch()[0] != undefined){
 			if(Users.find({username: loginattempt[0]}).fetch()[0].password == loginattempt[1]){
 				Session.set("currentuser",loginattempt[0]);
 			}else{
-				alert("Username/password not found - 1");
+				alert("Username/password not found");
 			}
 		}else{
-				alert("Username/password not found - 2");
+				alert("Username/password not found");
 		}
-		//to proceed with the login check
+		e.target.user.value ="";
+		e.target.pass.value ="";
 		return false;
 	}
 });
-
+//----create new blogs----------
 Template.blogcreate.events({
 	//Creating new blogs via the create form
 	"submit .blogcreate": function(e){
-	console.log("new blog: " + e.target.title.value);
+		//collect entry within array
 		var blogentry = [e.target.title.value,e.target.desc.value];
+		//finds the current user ID
 		var cUserID = Users.find({username:Session.get("currentuser")}).fetch()[0]._id
-//"R8CrNZbakfxMBvfpY"//Users.find({username: Session.get("currentuser")}).fetch()[0].userid;
+		//adds new blog to collection
 		Blogs.insert({title: blogentry[0], description: blogentry[1],datetime: Date(), userid: cUserID, username: Session.get("currentuser")});
-		
+		e.target.title.value ="";
+		e.target.desc.value = "";
 		return false;
 	}
 });
@@ -106,28 +118,37 @@ Template.blogcreate.events({
 Template.blogdisplay.events({
 	//removing the blog from the collection
 	"click #remove": function(e){
-	console.log(this._id);
+	//makes sure the the current user isn't noone
 		if(!Session.equals("currentuser","")){
+		//makes sure only the maker of the blog can remove it
 			if(Users.find({username:Session.get("currentuser")}).fetch()[0]._id == Blogs.findOne(this._id).userid){
-				console.log("removing collection item " + this._id);
-				Blogs.remove(this._id);
+				if(confirm("Are you sure you want to delete this blog?")){
+					Blogs.remove(this._id);
+				}
 			}
 		}
 	},
+	//edits blog with form
 	"submit .blogedit":function(e){
-		console.log(this._id);
+		//adds a new blog based on the form, from the user with the current date and time
 		Blogs.update(this._id,{title:e.target.title.value, description:e.target.desc.value, datetime: Date(),userid: Users.find({username:Session.get("currentuser")}).fetch()[0]._id, username: Session.get("currentuser")});
 		e.target.title.value =""; 
 		e.target.desc.value ="";
+		Session.set("editdisplay",false)
 		return false;
+	},
+	//display the edit form
+	'click #edit':function(e){
+		if(Session.get("editdisplay")){
+			Session.set("editdisplay",false);
+		}else{
+			Session.set("editdisplay",true);
+		}
+		
 	}
 	
 });
-/*Template.blogdisplay.events({
-	//editing the blog from the collection
-	
-});
-*/
+//----user registration section to toggle display of registration section
 Template.userregister.events({
 	'click .register': function(){
 		
@@ -137,11 +158,11 @@ Template.userregister.events({
 			Session.set('registerdisplay',false);
 		}
 	},
-
+//----registers new user----------
 	'submit .userregister':function(e){
-		console.log("register" + e.target.name.value + " : "+e.target.user.value+" : "+e.target.pass.value+" : "+e.target.desc.value);
-		
+		//collects user info from the form
 		var regattempt = [e.target.name.value,e.target.user.value,e.target.pass.value,e.target.desc.value];
+		//checks if the username already exists otherwise create user
 		if(Users.find({username: regattempt[1]}).fetch()[0] != undefined){
 			alert("user already exists");
 			e.target.name.value = "";
